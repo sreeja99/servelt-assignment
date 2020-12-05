2ee0803c9133b2425a82c57368a9c6166bf8f2b9
sreeja99 <srijagodishala@gmail.com>
2020-12-05 11:55:51 +0530

    Added:Checking Valid USerName And Password

--- src/com/bridgelabz/servletassignment/LoginServlet.java
+++ src/com/bridgelabz/servletassignment/LoginServlet.java
@@ -11,22 +11,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(description = "Login Servlet Testing", urlPatterns = { "/LoginServlet" }, initParams = {
-		@WebInitParam(name = "user", value = "Sreeja"), @WebInitParam(name = "password", value = "Warangal") })
+@WebServlet(description = "Login Servlet Testing", urlPatterns = { "/LoginServlet" })
 public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException{
 		String user = request.getParameter("user");
 		String pwd = request.getParameter("pwd");
-		String userId = getServletConfig().getInitParameter("user");
-		String password = getServletConfig().getInitParameter("password");
-		if (userId.equals(user) && password.equals(pwd)) {
+		if (user.matches("^[A-Z][a-zA-Z]{2,}$") && pwd.matches("^(?=\\S{8,})(?=\\S*[A-Z])(?=\\S*[\\d])[\\w]*[^\\w\\s][\\w]*")) {
 			request.setAttribute("user", user);
 			request.getRequestDispatcher("loginSuccess.jsp").forward(request, response);
 		} else {
 			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
 			PrintWriter out = response.getWriter();
-			out.println("<font color=red>Either user-name or password is wrong.</font>");
+			out.println("<font color=red>Either user-name or password is Invalid.</font>");
 			rd.include(request, response);
 		}
 	}
